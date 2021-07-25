f39fe41fa5ddaab8bf960fe5c32380f6f996f9a6
Christopher J. Brody <chris.brody+brodybits@gmail.com>
2021-07-25 19:11:15 -0400

    fix(evcore): cordova-sqlite-evcore-free-dependencies@0.11.1
    with SQLite 3.36.0 update
    
    as needed to avoid an issue with SQLite 3.35.x:
    
    - https://sqlite.org/src/info/c88f3036a2
      "ALTER TABLE DROP COLUMN corrupts data"
    
    as discussed in this comment in the SQLite forum:
    - https://sqlite.org/forum/forumpost/ac6086ecde66f1c4?t=h

--- spec/www/spec/sqlite-version-test.js
+++ spec/www/spec/sqlite-version-test.js
@@ -104,7 +104,7 @@ var mytests = function() {
               expect(rs).toBeDefined();
               expect(rs.rows).toBeDefined();
               expect(rs.rows.length).toBe(1);
-              expect(rs.rows.item(0).myResult).toBe('3.35.5');
+              expect(rs.rows.item(0).myResult).toBe('3.36.0');
 
               // Close (plugin only) & finish:
               (isWebSql) ? done() : db.close(done, done);
